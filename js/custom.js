e29cd264710ed9c11b9b3186f4012d336420c6f0
Sr Joe <srjoethef@gmail.com>
2021-10-13 22:17:34 +0530

    Login update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    document.getElementById("log-in").style.display = "initial";
+    document.getElementById("logged-in").style.display = "none";
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    
+    document.getElementById("log-in").style.display = "none";
+    document.getElementById("logged-in").style.display = "initial";
+  }
+});
+
 // to get current year
 function getYear() {
     var currentDate = new Date();
@@ -5,4 +25,12 @@ function getYear() {
     document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
+
+
 getYear();
+
+function login(){
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("pass_field").value;
+    window.alert(userEmail + " " + userPass);
+}
